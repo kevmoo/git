71040af955697c0c13e5924f1d10d352b142e38a
Kevin Moore <kevin@thinkpixellab.com>
2012-11-29 18:17:49 -0500

    added 'lerp' top level math function

--- lib/src/bot/math/math_functions.dart
+++ lib/src/bot/math/math_functions.dart
@@ -1,7 +1,22 @@
 part of bot;
 
+// TODO: bad to pollute top-level NS like this?
+// Another library? Hmm...
+
 bool isValidNumber(num value) {
   return value != null && !value.isInfinite && !value.isNaN;
+}
+
+/**
+ * Performs linear interpolation between values a and b. Returns the value
+ * between a and b proportional to x (when x is between 0 and 1. When x is
+ * outside this range, the return value is a linear extrapolation).
+ */
+lerp(num a, num b, num x) {
+  requireArgument(isValidNumber(a), 'a');
+  requireArgument(isValidNumber(b), 'b');
+  requireArgument(isValidNumber(x), 'x');
+  return a + x * (b - a);
 }
 
 math.Random get rnd {
