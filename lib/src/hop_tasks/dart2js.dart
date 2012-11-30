6c4d45f62b07163c1860f248b19ffb808d9c1cfb
Kevin Moore <kevin@thinkpixellab.com>
2012-11-29 21:06:51 -0500

    hop_tasks: whitespace

--- lib/src/hop_tasks/dart2js.dart
+++ lib/src/hop_tasks/dart2js.dart
@@ -3,8 +3,9 @@ part of hop_tasks;
 Task createDart2JsTask(List<String> inputs,
     {bool minify: false}) {
   return new Task.async((context) {
-    final futureFuncs = $(inputs).map((p) =>
-        () => _dart2js(context, p, minify: minify)).toList();
+    final futureFuncs = $(inputs)
+        .map((p) => () => _dart2js(context, p, minify: minify))
+        .toList();
     return _chainTasks(futureFuncs);
   }, 'Run Dart-to-Javascript compiler');
 }
@@ -43,8 +44,7 @@ Future<bool> _chainTasks(List<Func<Future<bool>>> futures, [int index=0]) {
   return future.chain((bool status) {
     if(status) {
       return _chainTasks(futures, index+1);
-    }
-    else {
+    } else {
       return new Future.immediate(false);
     }
   });
