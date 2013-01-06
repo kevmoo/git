38f4183aab36dc74ea341b4c48f3e85194d59ac0
Kevin Moore <kevin@thinkpixellab.com>
2013-01-06 17:23:40 -0500

    moved 'test' tests to shared -> works fine in browser

--- test/test_console.dart
+++ test/test_console.dart
@@ -2,10 +2,8 @@ library test_console;
 
 import 'hop/_hop.dart' as hop;
 import 'hop_tasks/_hop_tasks.dart' as hop_tasks;
-import 'test/_test.dart' as test;
 
 void register() {
   hop.register();
   hop_tasks.register();
-  test.register();
 }
--- test/test_shared.dart
+++ test/test_shared.dart
@@ -2,8 +2,10 @@ library test_shared;
 
 import 'async/_async_runner.dart' as async;
 import 'bot/_bot_runner.dart' as bot;
+import 'test/_test.dart' as test;
 
 void register() {
-  bot.register();
   async.register();
+  bot.register();
+  test.register();
 }
