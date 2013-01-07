866b15f1560f282f7f10f938f9ee935c2e8bdca5
Kevin Moore <kevin@thinkpixellab.com>
2013-01-07 09:24:13 -0500

    test: moving a few things around

--- lib/src/test/top_level.dart
+++ lib/src/test/top_level.dart
@@ -19,3 +19,32 @@ class _NullArgumentError extends TypeMatcher {
   const _NullArgumentError() : super("NullArgumentException");
   bool matches(item, MatchState matchState) => item is NullArgumentError;
 }
+
+void expectFutureFail(Future future, [Action1 onException]) {
+  assert(future != null);
+
+  final testWait = expectAsync1((Future f) {
+    assert(f.isComplete);
+
+    expect(f.hasValue, isFalse, reason: 'Expected future to throw an exception');
+    if(onException != null) {
+      onException(f.exception);
+    }
+  });
+  future.onComplete(testWait);
+}
+
+void expectFutureComplete(Future future, [Action1 onComplete]) {
+  assert(future != null);
+
+  final testWait = expectAsync1((Future f) {
+    assert(f.isComplete);
+
+    expect(f.hasValue, true, reason: 'Expected future to complete. Instead: ${f.exception}');
+
+    if(onComplete != null) {
+      onComplete(f.value);
+    }
+  });
+  future.onComplete(testWait);
+}
--- lib/test.dart
+++ lib/test.dart
@@ -3,6 +3,5 @@ library bot_test;
 import 'package:unittest/unittest.dart';
 import 'bot.dart';
 
-part 'src/test/async_test.dart';
 part 'src/test/event_watcher.dart';
 part 'src/test/top_level.dart';
