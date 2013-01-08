0386ed6b8b7f5b89c4afe3057fe20770c9a46f20
Kevin Moore <kevin@thinkpixellab.com>
2013-01-08 10:52:49 -0500

    bot_git: creating some classes

---
+++ lib/src/bot_git/git.dart
@@ -0,0 +1,20 @@
+part of bot_git;
+
+class Git {
+  static Future<ProcessResult> runGit(List<String> args, [bool throwOnError = true]) {
+    return Process.run('git', args)
+        .transform((ProcessResult pr) {
+          if(throwOnError) {
+            _throwIfProcessFailed(pr, 'git', args);
+          }
+          return pr;
+        });
+  }
+
+  static void _throwIfProcessFailed(ProcessResult pr, String process, List<String> args) {
+    assert(pr != null);
+    if(pr.exitCode != 0) {
+      throw new ProcessException('git', ['unknown'], pr.stderr.trim(), pr.exitCode);
+    }
+  }
+}
---
+++ lib/src/bot_git/git_dir.dart
@@ -0,0 +1,73 @@
+part of bot_git;
+
+class GitDir {
+  static final RegExp _shaRegExp = new RegExp(r'^[a-f0-9]{40}$');
+
+  final Path _path;
+
+  factory GitDir(String path) {
+    return new GitDir.fromPath(new Path(path));
+  }
+
+  GitDir.fromPath(Path path) :
+    this._path = path.canonicalize() {
+    assert(new Directory.fromPath(_path).existsSync());
+  }
+
+  Path get path => _path;
+
+  Future<List<String>> getBranches() {
+    return Git.runGit(['ls-remote', _path.toNativePath()])
+        .transform((ProcessResult pr) {
+          assert(pr.exitCode == 0);
+
+          // TODO: not working *at all* yet.
+          return [];
+        });
+  }
+
+  Future<Map<String, String>> writeObject(List<String> paths) {
+    final args = ['hash-object', '-t', 'blob', '-w', '--no-filters', '--'];
+    args.addAll(paths);
+    return _doGit(args)
+        .transform((ProcessResult pr) {
+          final val = pr.stdout.trim();
+          final shas = val.split(new RegExp(r'\s+'));
+          assert(shas.length == paths.length);
+          assert(shas.every((sha) => _shaRegExp.hasMatch(sha)));
+          final map = new Map<String, String>();
+          for(var i = 0; i < shas.length; i++) {
+            map[paths[i]] = shas[i];
+          }
+          return map;
+        });
+  }
+
+  Future<ProcessResult> _doGit(List<String> args, [bool throwOnError = true]) {
+    var allArgs = ['--git-dir', _gitDirPath]
+      ..addAll(args);
+    return Git.runGit(allArgs, throwOnError);
+  }
+
+  String get _gitDirPath => _path.append('.git').toNativePath();
+
+  static Future<GitDir> init(Directory source) {
+    assert(source.existsSync());
+
+    // first, verify it's empty
+    return IoHelpers.isEmpty(source)
+        .chain((bool isEmpty) {
+          if(!isEmpty) {
+            throw 'source Directory is not empty';
+          }
+          return _init(source);
+        });
+  }
+
+  static Future<GitDir> _init(Directory source) {
+    return Git.runGit(['init', source.path])
+        .transform((ProcessResult pr) {
+          return new GitDir(source.path);
+        });
+  }
+}
