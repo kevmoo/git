6f15665f123a95e1e191e7d232aa01dd27d76f28
Kevin Moore <kevin@thinkpixellab.com>
2013-01-29 16:15:04 -0500

    hop_tasks:branchForDir: better error, support for working dir

--- lib/src/hop_tasks/git.dart
+++ lib/src/hop_tasks/git.dart
@@ -1,50 +1,52 @@
 part of hop_tasks;
 
 Future<bool> branchForDir(TaskContext ctx, String sourceBranch, String sourceDir,
-    String targetBranch) {
+    String targetBranch, {String workingDir}) {
 
-  return Process.run('git', ['ls-tree', '-d', sourceBranch, sourceDir])
+  return _runGit(['ls-tree', '-d', sourceBranch, sourceDir], workingDir)
       .then(_getTreeFromLsTree)
       .then((String tree) =>
-          _fromSourceDirTree(ctx, tree, targetBranch, sourceDir, sourceBranch));
+          _fromSourceDirTree(ctx, tree, targetBranch, sourceDir, sourceBranch, workingDir));
 }
 
 String _getTreeFromLsTree(ProcessResult pr) {
-  require(pr.stdout.length > 0, 'Output was empty. Does the provided dir exist?');
+  if(pr.stdout.isEmpty) {
+    throw 'No output. Does the provided dir exist? Provided source Branch? Git error: ${pr.stderr}';
+  }
   return pr.stdout;
 }
 
 Future<bool> _fromSourceDirTree(TaskContext ctx, String tree,
-    String targetBranch, String sourceDir, String sourceBranch) {
+    String targetBranch, String sourceDir, String sourceBranch, String workingDir) {
   final split = tree.split(_whiteSpaceExp);
   final sha = split[2];
 
   final gitArgs = new List<String>.from(['commit-tree', sha]);
   final branchNameRef = 'refs/heads/$targetBranch';
 
-  return Process.run('git', ['rev-parse', '--verify', '-p', branchNameRef])
+  return _runGit(['rev-parse', '--verify', '-p', branchNameRef], workingDir)
       .then((ProcessResult rppr) {
         if(rppr.exitCode == 0) {
           // existing branch
           final parent = rppr.stdout.trim();
           return _withExistingBranch(ctx, parent, sha, sourceDir, gitArgs,
-              sourceBranch, branchNameRef, targetBranch);
+              sourceBranch, branchNameRef, targetBranch, workingDir);
         } else {
           // new branch
           return _getMasterCommit(ctx, 'created', gitArgs, sourceBranch,
-              sourceDir, branchNameRef, targetBranch);
+              sourceDir, branchNameRef, targetBranch, workingDir);
         }
       });
 }
 
 Future<bool> _withExistingBranch(TaskContext ctx, String parent, String dirSha,
     String sourceDir, List<String> gitArgs, String sourceBranch,
-    String branchNameRef, String targetBranch) {
-  return Process.run('git', ['cat-file', '-p', parent])
+    String branchNameRef, String targetBranch, String workingDir) {
+  return _runGit(['cat-file', '-p', parent], workingDir)
       .then(_getParentTree)
       .then((String parentTree) =>
           _continueWithExistingBranch(ctx, parent, parentTree, dirSha, sourceDir,
-              gitArgs, sourceBranch, branchNameRef, targetBranch));
+              gitArgs, sourceBranch, branchNameRef, targetBranch, workingDir));
 }
 
 String _getParentTree(ProcessResult pr) {
@@ -57,24 +59,25 @@ String _getParentTree(ProcessResult pr) {
 Future<bool> _continueWithExistingBranch(TaskContext ctx,
     String parent, String parentTree, String dirSha, String sourceDir,
     List<String> gitArgs, String sourceBranch, String branchNameRef,
-    String targetBranch) {
+    String targetBranch, workingDir) {
   if(parentTree == dirSha) {
     ctx.fine('There have been no changes to "$sourceDir" since the last commit');
     return new Future.immediate(true);
   } else {
     gitArgs.addAll(['-p', parent]);
     return _getMasterCommit(ctx, 'updated', gitArgs, sourceBranch, sourceDir,
-        branchNameRef, targetBranch);
+        branchNameRef, targetBranch, workingDir);
   }
 }
 
 Future<bool> _getMasterCommit(TaskContext ctx, String verb, List<String> gitArgs,
-    String sourceBranch, String sourceDir, String branchNameRef, String targetBranch) {
+    String sourceBranch, String sourceDir, String branchNameRef, String targetBranch,
+    String workingDir) {
 
-  return Process.run('git', ['rev-parse', sourceBranch])
+  return _runGit(['rev-parse', sourceBranch], workingDir)
       .then(_transformRevParse)
       .then((masterCommit) => _doCommitSimple(ctx, verb, gitArgs, sourceDir,
-          masterCommit, branchNameRef, targetBranch));
+          masterCommit, branchNameRef, targetBranch, workingDir));
 }
 
 String _transformRevParse(ProcessResult pr) {
@@ -84,26 +87,35 @@ String _transformRevParse(ProcessResult pr) {
 
 Future<bool> _doCommitSimple(TaskContext ctx, String verb, List<String> gitArgs,
     String sourceDir, String masterCommit, String branchNameRef,
-    String targetBranch) {
+    String targetBranch, String workingDir) {
 
   masterCommit = masterCommit.substring(0, 8);
 
   gitArgs.addAll(['-m', 'Contents of $sourceDir from commit $masterCommit']);
 
-  return Process.run('git', gitArgs)
+  return _runGit(gitArgs, workingDir)
       .then((ProcessResult pr) {
         require(pr.exitCode == 0, pr.stderr);
 
         final newCommitSha = pr.stdout.trim();
         ctx.info('Create new commit: $newCommitSha');
 
-        return Process.run('git', ['update-ref', branchNameRef, newCommitSha])
+        return _runGit(['update-ref', branchNameRef, newCommitSha], workingDir)
             .then((ProcessResult updateRefPr) {
               require(updateRefPr.exitCode == 0);
               ctx.info("Branch '$targetBranch' $verb");
               return true;
               });
       });
+}
+
+Future<ProcessResult> _runGit(List<String> args, String workingDir) {
+  final options = new ProcessOptions();
+  if(workingDir != null) {
+    options.workingDirectory = workingDir;
+  }
+
+  return Process.run('git', args, options);
 }
 
 final _whiteSpaceExp = new RegExp(r'\s+', multiLine: true);
