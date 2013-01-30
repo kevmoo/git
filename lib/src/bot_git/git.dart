75210c04a32a55e6daed3b30b94999b17576aa67
Kevin Moore <kevin@thinkpixellab.com>
2013-01-30 17:22:50 -0500

    bot_git: TreeEntry

--- lib/src/bot_git/git.dart
+++ lib/src/bot_git/git.dart
@@ -60,6 +60,9 @@ ${pr.stderr}''';
   }
 }
 
+/**
+ * Represents the output from `git ls-remote`
+ */
 class GitReference {
   final String sha;
   final String reference;
@@ -91,4 +94,41 @@ class BranchReference extends GitReference {
 
   BranchReference._internal(String sha, String reference, this.branchName) :
     super(sha, reference);
+}
+
+class TreeEntry {
+  static final _lsTreeLine = r'^([0-9]{6}) (blob|tree) ('
+      .concat(Git._shaRegexPattern)
+      .concat(')\t(\\S.*\\S)\$');
+
+  static final _lsTreeRegEx = new RegExp(_lsTreeLine);
+
+  String mode;
+
+  // TODO: enum for type?
+  String type;
+  String sha;
+  String name;
+
+  TreeEntry(this.mode, this.type, this.sha, this.name) {
+    // TODO: asserts!
+  }
+
+  factory TreeEntry.fromLsTree(String value) {
+    // TODO: should catch and re-throw a descriptive error
+    final match = _lsTreeRegEx.allMatches(value).single;
+
+    return new TreeEntry(match[1], match[2], match[3], match[4]);
+  }
+
+  static List<TreeEntry> fromLsTreeOutput(String output) {
+    final lines = Util.splitLines(output);
+
+    // last line should be empty
+    assert(lines.last.length == 0);
+
+    return lines.getRange(0, lines.length-1)
+        .mappedBy((line) => new TreeEntry.fromLsTree(line))
+        .toList();
+  }
 }
