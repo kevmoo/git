54aa821cd27d8250f0fdaf5c5eb94e40d9438cf6
Kevin Moore <kevin@thinkpixellab.com>
2013-01-31 09:22:24 -0500

    renaming in hop_tasks for clarity

--- lib/hop_tasks.dart
+++ lib/hop_tasks.dart
@@ -11,5 +11,5 @@ import 'package:unittest/unittest.dart' as unittest;
 part 'src/hop_tasks/unit_test.dart';
 part 'src/hop_tasks/process.dart';
 part 'src/hop_tasks/dart2js.dart';
-part 'src/hop_tasks/git.dart';
+part 'src/hop_tasks/git_tasks.dart';
 part 'src/hop_tasks/dartdoc.dart';
