7e4c41679b3b9a0cf5d165d073bad415e7ca500a
Kevin Moore <kevin@thinkpixellab.com>
2013-01-31 10:33:48 -0500

    hop_tasks: git: down to three methods. Much better.

--- lib/src/hop_tasks/git_tasks.dart
+++ lib/src/hop_tasks/git_tasks.dart
@@ -9,33 +9,35 @@ Future<bool> branchForDir(TaskContext ctx, String sourceBranch, String sourceDir
 
   final gitDir = new GitDir(workingDir);
 
+  List<String> gitArgs;
+  String sourceDirTreeSha;
+
   return gitDir.lsTree(sourceBranch, subTreesOnly: true, path: sourceDir)
       .then((List<TreeEntry> entries) {
         assert(entries.length <= 1);
         if(entries.isEmpty) {
           throw 'Could not find a matching dir on the provided branch';
         }
-        return _fromSourceDirTree(ctx, entries.single, targetBranch, sourceDir, sourceBranch, gitDir);
-      });
-}
 
-Future<bool> _fromSourceDirTree(TaskContext ctx, TreeEntry tree,
-    String targetBranch, String sourceDir, String sourceBranch, GitDir gitDir) {
-  final sha = tree.sha;
+        final tree = entries.single;
 
-  final gitArgs = new List<String>.from(['commit-tree', sha]);
+        sourceDirTreeSha = tree.sha;
 
-  return gitDir.getBranchReference(targetBranch)
+        gitArgs = ['commit-tree', sourceDirTreeSha];
+
+        return gitDir.getBranchReference(targetBranch);
+      })
       .then((BranchReference branchRef) {
 
         if(branchRef == null) {
           // branch does not exist. New branch!
-          return _goCommit(ctx, 'created', gitArgs, sourceBranch,
-              sourceDir, targetBranch, gitDir);
+          return _doCommit(ctx, 'created', gitArgs, sourceBranch, sourceDir,
+              targetBranch, gitDir);
         } else {
-          // existing branch
-          return _withExistingBranch(ctx, branchRef, sha, sourceDir, gitArgs,
-              sourceBranch, targetBranch, gitDir);
+          // existing branch, need to find the tree info so we can create
+          // a valid commit w/ the right parent
+          return _withExistingBranch(ctx, branchRef, sourceDirTreeSha, sourceDir,
+              gitArgs, sourceBranch, targetBranch, gitDir);
         }
       });
 }
@@ -43,31 +45,21 @@ Future<bool> _fromSourceDirTree(TaskContext ctx, TreeEntry tree,
 Future<bool> _withExistingBranch(TaskContext ctx, BranchReference targetBranchRef, String dirSha,
     String sourceDir, List<String> gitArgs, String sourceBranch, String targetBranch, GitDir gitDir) {
 
-  final lastCommitSha = targetBranchRef.sha;
-
-  return gitDir.getCommit(lastCommitSha)
-      .then((Commit commitObj) =>
-          _continueWithExistingBranch(ctx, lastCommitSha, commitObj.treeSha, dirSha, sourceDir,
-              gitArgs, sourceBranch, targetBranch, gitDir));
+  return gitDir.getCommit(targetBranchRef.sha)
+      .then((Commit commitObj) {
+        if(commitObj.treeSha == dirSha) {
+          ctx.fine('There have been no changes to "$sourceDir" since the last commit');
+          return new Future.immediate(true);
+        } else {
+          gitArgs.addAll(['-p', targetBranchRef.sha]);
+          return _doCommit(ctx, 'updated', gitArgs, sourceBranch, sourceDir,
+              targetBranch, gitDir);
+        }
+      });
 }
 
-Future<bool> _continueWithExistingBranch(TaskContext ctx,
-    String parent, String parentTree, String dirSha, String sourceDir,
-    List<String> gitArgs, String sourceBranch, String targetBranch, GitDir gitDir) {
-  if(parentTree == dirSha) {
-    ctx.fine('There have been no changes to "$sourceDir" since the last commit');
-    return new Future.immediate(true);
-  } else {
-    gitArgs.addAll(['-p', parent]);
-    return _goCommit(ctx, 'updated', gitArgs, sourceBranch, sourceDir,
-        targetBranch, gitDir);
-  }
-}
-
-Future<bool> _goCommit(TaskContext ctx, String verb, List<String> gitArgs,
+Future<bool> _doCommit(TaskContext ctx, String verb, List<String> gitArgs,
     String sourceBranch, String sourceDir, String targetBranch, GitDir gitDir) {
-
-  final branchNameRef = 'refs/heads/$targetBranch';
 
   return gitDir.getBranchReference(sourceBranch)
       .then((BranchReference branchRef) {
@@ -83,6 +75,8 @@ Future<bool> _goCommit(TaskContext ctx, String verb, List<String> gitArgs,
 
         final newCommitSha = pr.stdout.trim();
         ctx.info('Create new commit: $newCommitSha');
+
+        final branchNameRef = 'refs/heads/$targetBranch';
 
         return gitDir.runCommand(['update-ref', branchNameRef, newCommitSha]);
       })
