1b9e186f6234cfcf835a8596533eb660e50dbb22
Kevin Moore <kevin@thinkpixellab.com>
2013-01-31 20:10:04 -0500

    Removed Dart copyright

--- bin/run_browser_test_drt.sh
+++ bin/run_browser_test_drt.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-# Copyright (c) 2013, the Dart project authors.  Please see the LICENSE file
-# for details. All rights reserved. Use of this source code is governed by a
-# MIT-style license that can be found in the LICENSE file.
 
 # bail on error
 set -e
