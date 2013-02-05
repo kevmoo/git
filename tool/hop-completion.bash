fe02247dc24a1e155852e2a491c5f6e1d726305b
Kevin Moore <kevin@thinkpixellab.com>
2013-02-05 18:46:51 -0500

    Handle auto-complete attempts outside a 'hop' directory
    Closes #33

--- tool/hop-completion.bash
+++ tool/hop-completion.bash
@@ -11,7 +11,15 @@ _hop()
   local cur prev opts
   COMPREPLY=()
   cur="${COMP_WORDS[COMP_CWORD]}"
-  opts=`hop print_raw_task_list`
+
+  # capture both stdout and stderr
+  opts=`hop print_raw_task_list 2>&1`
+
+  # if exit code for `hop` is not 0, then return an error
+  # probably not in a directory with tool/hop_runner.dart
+  if [ $? -ne 0 ] ; then 
+    return 1
+  fi
 
   if [[ ${cur} == -* || ${COMP_CWORD} -eq 1 ]] ; then
       COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
