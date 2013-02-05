66c1e1616e85bf0e366b02a774b7307ba3bef4f2
Kevin Moore <kevin@thinkpixellab.com>
2013-02-04 21:57:09 -0500

    hop_tasks: analyzer: a bit more simple

--- lib/src/hop_tasks/dart_analyzer.dart
+++ lib/src/hop_tasks/dart_analyzer.dart
@@ -3,36 +3,6 @@
 
 part of hop_tasks;
 
-class _AnalyzerResult {
-  final ProcessResult processResult;
-  final Path path;
-
-  _AnalyzerResult(this.path, this.processResult);
-
-  String toString() {
-    return "${_getPrefix(processResult.exitCode)}: $path";
-  }
-
-  String toVerboseString() {
-    return '''${toString()}
-${processResult.stdout}
-${processResult.stderr}''';
-  }
-
-  static String _getPrefix(int exitCode) {
-    switch(exitCode) {
-      case 0:
-        return "PASSED";
-      case 1:
-        return "WARNING";
-      case 2:
-        return "ERROR";
-      default:
-        return "Unknown exit code $exitCode";
-    }
-  }
-}
-
 Task createDartAnalyzerTask(Iterable<String> files) {
   return new Task.async((context) {
 
@@ -56,23 +26,47 @@ ArgParser _getDartAnalyzerParser() {
 
 Future<bool> _processAnalyzerFile(TaskContext context, List<Path> analyzerFilePaths,
     bool enableTypeChecks, bool verbose) {
-  final results = new List<_AnalyzerResult>();
+
+  int errorsCount = 0;
+  int passedCount = 0;
+  int warningCount = 0;
 
   return Future.forEach(analyzerFilePaths, (Path path) {
-    return _analyzer(context, path, enableTypeChecks)
-        .then((_AnalyzerResult ar) {
-          if(verbose) {
-            context.info(ar.toVerboseString());
-          } else {
-            context.info(ar.toString());
+    return _analyzer(context, path, enableTypeChecks, verbose)
+        .then((int exitCode) {
+
+          String prefix;
+
+          switch(exitCode) {
+            case 0:
+              prefix = "PASSED";
+              passedCount++;
+              break;
+            case 1:
+              prefix = "WARNING";
+              warningCount++;
+              break;
+            case 2:
+              prefix =  "ERROR";
+              errorsCount++;
+              break;
+            default:
+              prefix = "Unknown exit code $exitCode";
+              errorsCount++;
+              break;
           }
-          results.add(ar);
+
+          context.info("$prefix - $path");
         });
-    }).
-    then((_) => _processResults(context, results, verbose));
+    })
+    .then((_) {
+      context.info("PASSED: ${passedCount}, WARNING: ${warningCount}, ERROR: ${errorsCount}");
+      return errorsCount == 0;
+    });
 }
 
-Future<_AnalyzerResult> _analyzer(TaskContext context, Path filePath, bool enableTypeChecks) {
+Future<int> _analyzer(TaskContext context, Path filePath, bool enableTypeChecks,
+    bool verbose) {
   TempDir tmpDir;
 
   return TempDir.create()
@@ -87,19 +81,15 @@ Future<_AnalyzerResult> _analyzer(TaskContext context, Path filePath, bool enabl
 
         processArgs.addAll([filePath.toNativePath()]);
 
-        return Process.run('dart_analyzer', processArgs);
+        return Process.start('dart_analyzer', processArgs);
       })
-      .then((ProcessResult processResult) {
-        if (processResult.exitCode == 127) {
-          // Check for exit code, something went wrong here.
-          var sb = new StringBuffer();
-          sb.add(processResult.stderr);
-          sb.add(processResult.stdout);
-          sb.add("Exit Code 127");
-          throw sb.toString();
+      .then((process) {
+        if(verbose) {
+          return pipeProcess(process,
+              stdOutWriter: context.fine,
+              stdErrWriter: context.severe);
         } else {
-          var analyzerResult = new _AnalyzerResult(filePath, processResult);
-          return analyzerResult;
+          return pipeProcess(process);
         }
       })
       .whenComplete(() {
@@ -107,34 +97,4 @@ Future<_AnalyzerResult> _analyzer(TaskContext context, Path filePath, bool enabl
           tmpDir.dispose();
         }
       });
-}
-
-bool _processResults(TaskContext context, List<_AnalyzerResult> analyzerResults, bool verbose) {
-  final finalResults = new StringBuffer();
-  int errorsCount = 0;
-  int passedCount = 0;
-  int warningCount = 0;
-
-  analyzerResults.forEach((_AnalyzerResult result) {
-    /*
-     *  --extended-exit-code : 0 - clean; 1 - has warnings; 2 - has errors
-     */
-
-    switch(result.processResult.exitCode) {
-      case 0:
-        passedCount++;
-        break;
-      case 1:
-        warningCount++;
-        break;
-      case 2:
-      default:
-        errorsCount++;
-        break;
-    }
-  });
-
-  context.info("PASSED: ${passedCount}, WARNING: ${warningCount}, ERROR: ${errorsCount}");
-
-  return errorsCount == 0;
 }
