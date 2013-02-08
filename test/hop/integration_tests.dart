09f6bbbf1b51ba9bdf718d8a0599c04ccd207d9b
Kevin Moore <kevin@thinkpixellab.com>
2013-02-08 09:14:41 -0500

    test: hop integration tests now pass on Windows

--- test/hop/integration_tests.dart
+++ test/hop/integration_tests.dart
@@ -1,29 +1,33 @@
 part of test_hop;
 
 class IntegrationTests {
-  static final hopPath = 'bin/hop';
-
   static void run() {
     test('hop output is sorted', _testOutputSorted);
     test('bad hop command', _testBadHopCommand);
   }
 
   static void _testBadHopCommand() {
-    final future = Process.run(hopPath, ['bad_command_name'])
-        .then((ProcessResult pr) {
-          expect(pr.exitCode, equals(RunResult.BAD_USAGE.exitCode));
-        });
-
-    expect(future, completes);
+    _runHop(['bad_command_name'], (ProcessResult pr) {
+      expect(pr.exitCode, equals(RunResult.BAD_USAGE.exitCode));
+    });
   }
 
   static void _testOutputSorted() {
-    final future = Process.run(hopPath, [Runner.RAW_TASK_LIST_CMD])
-        .then((ProcessResult pr) {
-          expect(pr.exitCode, equals(RunResult.SUCCESS.exitCode));
-          final lines = pr.stdout.trim().split('\n');
-          expect(lines, orderedEquals(['analyze_libs', 'analyze_test_libs', 'bench', 'dart2js', 'docs', 'hello', 'test']));
-        });
+    _runHop([Runner.RAW_TASK_LIST_CMD], (ProcessResult pr) {
+      expect(pr.exitCode, equals(RunResult.SUCCESS.exitCode));
+      final lines = Util.splitLines(pr.stdout.trim()).toList();
+      expect(lines, orderedEquals(['analyze_libs', 'analyze_test_libs', 'bench', 'dart2js', 'docs', 'hello', 'test']));
+    });
+  }
+
+  static void _runHop(Iterable<String> args, Action1<ProcessResult> handler) {
+    final list = args.toList();
+    
+    final hopRunnerPath = 'tool/hop_runner.dart';
+    list.insertRange(0, 1, hopRunnerPath);
+       
+    final future = Process.run('dart', list)
+        .then(handler);
 
     expect(future, completes);
   }
