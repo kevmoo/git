84f80bfcc13f66309056d40d8affccf92bad0582
Kevin Moore <kevin@thinkpixellab.com>
2013-02-09 15:20:44 -0500

    hop_tasks: dartdoc: learned optional `excludeLibs` and `linkApi` args

--- lib/src/hop_tasks/dartdoc.dart
+++ lib/src/hop_tasks/dartdoc.dart
@@ -3,13 +3,18 @@ part of hop_tasks;
 // TODO: add post-build options to pretty-up the docs
 
 Task getCompileDocsFunc(String targetBranch, String packageDir,
-                        Func<Future<List<String>>> libGetter) {
-  return new Task.async((ctx) => compileDocs(ctx, targetBranch, libGetter, packageDir),
+                        Func<Future<List<String>>> libGetter,
+                        {Iterable<String> excludeLibs, bool linkApi: false}) {
+  return new Task.async((ctx) => compileDocs(ctx, targetBranch, libGetter,
+      packageDir, excludeLibs: excludeLibs, linkApi: linkApi),
       'Generate documentation for the provided libraries.');
 }
 
 Future<bool> compileDocs(TaskContext ctx, String targetBranch,
-    Func<Future<List<String>>> libGetter, String packageDir) {
+    Func<Future<List<String>>> libGetter, String packageDir,
+    {Iterable<String> excludeLibs, bool linkApi: false}) {
+
+  final excludeList = excludeLibs == null ? [] : excludeLibs.toList();
 
   final parser = _getDartDocParser();
   final parseResult = _helpfulParseArgs(ctx, parser, ctx.arguments);
@@ -42,7 +47,7 @@ Future<bool> compileDocs(TaskContext ctx, String targetBranch,
       .then((String commitMessage) {
 
         return gitDir.populateBranch(targetBranch,
-            (TempDir td) => _doDocsPopulate(ctx, td, libs, packageDir),
+            (TempDir td) => _doDocsPopulate(ctx, td, libs, packageDir, excludeList, linkApi),
             commitMessage);
       })
       .then((Commit value) {
@@ -54,18 +59,6 @@ Future<bool> compileDocs(TaskContext ctx, String targetBranch,
         }
 
         return true;
-      });
-}
-
-Future _doDocsPopulate(TaskContext ctx, TempDir dir, Collection<String> libs, String packageDir) {
-  return _dartDoc(ctx, dir, libs, packageDir)
-      .then((bool dartDocSuccess) {
-        if(!dartDocSuccess) {
-          ctx.fail('The dartdoc process failed.');
-        }
-
-        // yeah, silly. ctx.fail should blow up. Should not get heer
-        assert(dartDocSuccess);
       });
 }
 
@@ -88,14 +81,31 @@ Future<String> _getCommitMessageFuture(GitDir gitDir) {
     });
 }
 
-Future<bool> _dartDoc(TaskContext ctx, TempDir outputDir, Collection<String> libs,
-    String packageDir) {
-  final args = ['--pkg', packageDir, '--omit-generation-time', '--out', outputDir.path, '--verbose'];
+Future _doDocsPopulate(TaskContext ctx, TempDir dir, Collection<String> libs,
+                       String packageDir, List<String> excludeList, bool linkApi) {
+  final args = ['--pkg', packageDir, '--omit-generation-time', '--out', dir.path, '--verbose'];
+
+  if(linkApi) {
+    args.add('--link-api');
+  }
+
+  if(!excludeList.isEmpty) {
+    args.add('--exclude-lib');
+    args.add(excludeList.join(','));
+  }
 
   args.addAll(libs);
-  ctx.fine("Generating docs into: $outputDir");
+  ctx.fine("Generating docs into: $dir");
 
   final sublogger = ctx.getSubLogger('dartdoc');
 
-  return startProcess(sublogger, "dartdoc", args);
+  return startProcess(sublogger, "dartdoc", args)
+      .then((bool dartDocSuccess) {
+        if(!dartDocSuccess) {
+          ctx.fail('The dartdoc process failed.');
+        }
+
+        // yeah, silly. ctx.fail should blow up. Should not get heer
+        assert(dartDocSuccess);
+      });
 }
