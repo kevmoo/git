181193eb17bee49c635fd0dc471ba3fadad13b6f
Kevin Moore <kevin@thinkpixellab.com>
2013-02-14 09:26:04 -0800

    hop: RunResult: include descriptive name

--- lib/src/hop/run_result.dart
+++ lib/src/hop/run_result.dart
@@ -8,25 +8,26 @@ class RunResult {
   // Accessed 2012-11-07
 
   /// Ran successfully
-  static final RunResult SUCCESS = const RunResult._internal(0);
+  static final RunResult SUCCESS = const RunResult._internal(0, 'SUCCESS');
 
   /// C/C++ standard for bad usage. Hop was called incorrectly
-  static final RunResult BAD_USAGE = const RunResult._internal(64);
+  static final RunResult BAD_USAGE = const RunResult._internal(64, 'BAD USAGE');
 
   /// Task was was started and failed gracefully
-  static final RunResult FAIL = const RunResult._internal(80);
+  static final RunResult FAIL = const RunResult._internal(80, 'FAIL');
 
   /// Task was was started, but threw an exception
-  static final RunResult EXCEPTION = const RunResult._internal(81);
+  static final RunResult EXCEPTION = const RunResult._internal(81, 'EXCEPTION');
 
   /// Task misbehaved. Returned null, used TaskContext incorrectly, etc.
-  static final RunResult ERROR = const RunResult._internal(82);
+  static final RunResult ERROR = const RunResult._internal(82, 'ERROR');
 
   final int exitCode;
+  final String name;
 
-  const RunResult._internal(this.exitCode);
+  const RunResult._internal(this.exitCode, this.name);
 
   bool get success => exitCode == SUCCESS.exitCode;
 
-  String toString() => 'RunResult - $exitCode';
+  String toString() => 'RunResult $name ($exitCode)';
 }
