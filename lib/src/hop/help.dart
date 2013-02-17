60b884973d07d15e04a980fe2210fd98422d263c
Kevin Moore <kevin@thinkpixellab.com>
2013-02-17 01:33:59 -0500

    hop getHelpTask: prints out help content for all tasks

--- lib/src/hop/help.dart
+++ lib/src/hop/help.dart
@@ -2,12 +2,51 @@ part of hop;
 
 Task getHelpTask() {
   return new Task.sync((_SubTaskContext ctx) {
+    final args = ctx.arguments;
+
+    if(args.command != null) {
+      _printHelpForTask(_sharedConfig, ctx._parent, args.command.name);
+      return true;
+    }
+
     _printHelp(_sharedConfig, ctx._parent);
+
+    if(!args.rest.isEmpty) {
+      ctx.severe('Not sure how to give help for: ${args.rest}');
+      return false;
+    }
+
     return true;
-  }, description: 'Print help information about available tasks');
+  },
+  description: 'Print help information about available tasks',
+  config: (parser) => _helpParserConfig(_sharedConfig, parser));
+}
+
+void _printHelpForTask(BaseConfig config, RootTaskContext ctx, String taskName) {
+  final task = config._getTask(taskName);
+  assert(task != null);
+
+  ctx.log(taskName, AnsiColor.BLUE);
+  ctx.log(task.description);
+  ctx.log('');
+
+  final usage = task.getUsage();
+  if(!usage.isEmpty) {
+    ctx.log(task.getUsage());
+    ctx.log('');
+  }
+}
+
+void _helpParserConfig(BaseConfig config, ArgParser parser) {
+  config.requireFrozen();
+
+  for(final taskName in config.taskNames) {
+    parser.addCommand(taskName);
+  }
 }
 
 void _printHelp(BaseConfig config, RootTaskContext ctx) {
+  config.requireFrozen();
   ctx.log('Welcome to HOP', AnsiColor.BLUE);
   ctx.log('');
   ctx.log('Tasks:', AnsiColor.BLUE);
@@ -20,6 +59,7 @@ void _printHelp(BaseConfig config, RootTaskContext ctx) {
 }
 
 void _printTaskTable(BaseConfig config, RootTaskContext ctx) {
+  config.requireFrozen();
   final columns = [
                    new ColumnDefinition('name', (name) => name),
                    new ColumnDefinition('description', (name) {
