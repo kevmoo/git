7d3e92857de2536116a133d3557a34056396d50e
Kevin Moore <kevin@thinkpixellab.com>
2013-02-20 15:49:52 -0500

    bot_git: added some arg validation to TreeEntry ctor
    and a doc comment on mode

--- lib/src/bot_git/git.dart
+++ lib/src/bot_git/git.dart
@@ -275,6 +275,11 @@ class TreeEntry {
 
   static final _lsTreeRegEx = new RegExp(_lsTreeLine);
 
+  /**
+   * All numbers.
+   *
+   * See this this [post on stackoverflow](http://stackoverflow.com/questions/737673/how-to-read-the-mode-field-of-git-ls-trees-output)
+   */
   String mode;
 
   // TODO: enum for type?
@@ -283,7 +288,15 @@ class TreeEntry {
   String name;
 
   TreeEntry(this.mode, this.type, this.sha, this.name) {
-    // TODO: asserts!
+    // TODO:
+    requireArgumentContainsPattern(new RegExp(r'^[0-9]{6}$'), mode, 'mode');
+
+    // TODO: enum or whitelist here
+    requireArgumentContainsPattern(new RegExp(r'^[a-z]+$'), type, 'type');
+    requireArgumentValidSha1(sha, 'sha');
+
+    // TODO: how can we be more careful here? no paths? hmm...
+    requireArgumentNotNullOrEmpty(name, 'name');
   }
 
   factory TreeEntry.fromLsTree(String value) {
