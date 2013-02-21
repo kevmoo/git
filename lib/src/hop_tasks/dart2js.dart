c099dd2fcc932419725d5caeaede1e379d0163bb
Kevin Moore <kevin@thinkpixellab.com>
2013-02-21 09:38:55 -0500

    hop_tasks: first param is now dynamic delayedRootList
    Not a breaking change. Now accepts combination of future or func...
    
    Also using Future.forEach instead of homegrown solution

--- lib/src/hop_tasks/dart2js.dart
+++ lib/src/hop_tasks/dart2js.dart
@@ -1,19 +1,42 @@
 part of hop_tasks;
 
-Task createDart2JsTask(List<String> inputs, {String output: null,
+/**
+ * [delayedRootList] a [List<String>] mapping to paths to libraries or some
+ * combinations of [Future] or [Function] values that return a [List<String>].
+ */
+Task createDart2JsTask(dynamic delayedRootList, {String output: null,
   String packageRoot: null, bool minify: false, bool allowUnsafeEval: true,
   bool liveTypeAnalysis: true, bool rejectDeprecatedFeatures: false}) {
+
   return new Task.async((context) {
-    final futureFuncs = inputs
-        .map((p) => () => _dart2js(context, p,
-            output: output,
-            minify: minify,
-            allowUnsafeEval: allowUnsafeEval,
-            packageRoot: packageRoot,
-            liveTypeAnalysis: liveTypeAnalysis,
-            rejectDeprecatedFeatures: rejectDeprecatedFeatures))
-        .toList();
-    return _chainTasks(context, futureFuncs);
+    bool errors = false;
+
+    return getDelayedResult(delayedRootList)
+        .then((List<String> inputs) {
+
+          return Future.forEach(inputs, (path) {
+            if(errors) {
+              context.warning('Compile errors. Skipping $path');
+              return new Future.immediate(null);
+            }
+
+            return _dart2js(context, path,
+                output: output,
+                minify: minify,
+                allowUnsafeEval: allowUnsafeEval,
+                packageRoot: packageRoot,
+                liveTypeAnalysis: liveTypeAnalysis,
+                rejectDeprecatedFeatures: rejectDeprecatedFeatures)
+                .then((bool success) {
+                  // should not have been run if we had pending errors
+                  assert(errors == false);
+                  errors = !success;
+                });
+          });
+        })
+        .then((_) {
+          return !errors;
+        });
   }, description: 'Run Dart-to-Javascript compiler');
 }
 
@@ -55,24 +78,4 @@ Future<bool> _dart2js(TaskContext ctx, String file, {String output: null,
   }
 
   return startProcess(ctx, 'dart2js', args);
-}
-
-Future<bool> _chainTasks(TaskContext ctx, List<Func<Future<bool>>> futures, [int index=0]) {
-  assert(index >= 0);
-  assert(index <= futures.length);
-  if(futures.length == 0) {
-    ctx.fail("No source files provided.");
-  }
-  if(index == futures.length) {
-    return new Future.immediate(true);
-  }
-  final func = futures[index];
-  final future = func();
-  return future.then((bool status) {
-    if(status) {
-      return _chainTasks(ctx, futures, index+1);
-    } else {
-      return false;
-    }
-  });
 }
