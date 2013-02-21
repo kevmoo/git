94a2056ca57570d18e1d20ea32714b8f4535322b
Kevin Moore <kevin@thinkpixellab.com>
2013-02-21 13:26:36 -0500

    BREAKING: hop: Task name can now contain '-' but cannot end in '-' or '_'

--- test/hop/task_list_tests.dart
+++ test/hop/task_list_tests.dart
@@ -11,15 +11,15 @@ class TaskListTests {
 
     test('reject bad task names', () {
       final tasks = new BaseConfig();
-      final goodNames = const['a','aa','a_','a1','a_b','a_cool_test1_'];
+      final goodNames = const['a','aa','a_b','a1','a_9','a_cool_test1', 'a-cool', 'a-9'];
 
       for(final n in goodNames) {
         tasks.addSync(n, (ctx) => true);
       }
 
-      final badNames = const['', null, ' start white', '1 start num', '\rtest',
+      final badNames = const['', null, ' start white', '1task', '1 start num', '\rtest',
                              'end_white ', 'contains white', 'contains\$bad',
-                             'test\r\test', 'UpperCase', 'camelCase'];
+                             'test\r\test', 'UpperCase', 'camelCase', 'a_', 'a-'];
 
       for(final n in badNames) {
         expect(() => tasks.addSync(n, (ctx) => true), throwsArgumentError);
