73a5e7d6e5423f27fcf34d52857dee93133ca563
Kevin Moore <kevin@thinkpixellab.com>
2013-02-23 13:50:15 -0500

    shell completion: allow generation of one script to complete multiple commands

--- bin/shell_completion_generator.dart
+++ bin/shell_completion_generator.dart
@@ -2,6 +2,7 @@
 
 import 'dart:async';
 import 'dart:io';
+import 'package:bot/bot.dart';
 
 const _templateName = 'command-completion.sh.template';
 const _binNameReplacement = '{{binName}}';
@@ -29,7 +30,7 @@ final _binNameMatch = new RegExp(r'^[a-zA-Z0-9]((\w|-|\.)*[a-zA-Z0-9])?$');
 void main() {
   final options = new Options();
 
-  String binName;
+  final binNames = new List<String>();
   File templateFile;
 
   new Future.immediate(options.script)
@@ -39,20 +40,18 @@ void main() {
       }
 
       if(options.arguments.isEmpty) {
-        throw 'Provide the name of the command';
+        throw 'Provide the of at least of one command';
       }
 
-      if(options.arguments.length > 1) {
-        throw 'Provide only one argument';
+      for(final binName in options.arguments) {
+        if(!_binNameMatch.hasMatch(binName)) {
+          final msg = 'The provided name - "$binName" - is invalid\n'
+              .concat('It must match regex: ${_binNameMatch.pattern}');
+          throw msg;
+        }
       }
 
-      binName = options.arguments.single;
-
-      if(!_binNameMatch.hasMatch(binName)) {
-        final msg = 'The provided name - "$binName" - is invalid\n'
-            .concat('It must match regex: ${_binNameMatch.pattern}');
-        throw msg;
-      }
+      binNames.addAll(options.arguments);
 
       final scriptFile = new File(scriptPath);
       return scriptFile.directory();
@@ -73,22 +72,52 @@ void main() {
       return templateFile.readAsString();
     })
     .then((String templateContents) {
-      templateContents = templateContents.replaceAll(_binNameReplacement, binName);
+      final prefix = Util.splitLines(_prefix).map((l) => '# $l').join('\n');
+      print(prefix);
 
-      var funcName = binName.replaceAll('.', '_');
-      funcName = '__${funcName}_completion';
-      templateContents = templateContents.replaceAll(_funcNameReplacement, funcName);
+      // empty line
+      print('');
+
+      for(final binName in binNames) {
+        _printBinName(templateContents, binName);
+      }
 
       final detailLines = ['Generated ${new DateTime.now().toUtc()}', 'By ${options.script}'];
 
       final details = detailLines.map((l) => '## $l').join('\n');
-      templateContents = templateContents.replaceAll(_scriptDetailsReplacement, details);
+      print(details);
 
-      print(templateContents);
+      // and a final newline
+      print('');
     })
     .catchError((AsyncError error) {
       print(error.error);
       exit(1);
     });
 }
+
+void _printBinName(String templateContents, String binName) {
+  templateContents = templateContents.replaceAll(_binNameReplacement, binName);
+
+  var funcName = binName.replaceAll('.', '_');
+  funcName = '__${funcName}_completion';
+  templateContents = templateContents.replaceAll(_funcNameReplacement, funcName);
+
+  print(templateContents);
+}
+
+const _prefix = '''
+
+Installation:
+
+Via shell config file  ~/.bashrc  (or ~/.zshrc)
+
+  Append the contents to config file
+  'source' the file in the config file
+
+You may also have a directory on your system that is configured
+   for completion files, such as:
+
+   /usr/local/etc/bash_completion.d/
+''';
 
