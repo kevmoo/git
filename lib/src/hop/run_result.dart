215d87628740d0dae62cff130dc8cf8e7f9e8962
Kevin Moore <kevin@thinkpixellab.com>
2013-02-28 16:52:29 -0500

    hop: RunResult: better doc comments

--- lib/src/hop/run_result.dart
+++ lib/src/hop/run_result.dart
@@ -1,25 +1,45 @@
 part of hop;
 
-// TODO: add some names and descriptions so these are more easily
-// understood at runtime
-
+/**
+ * For explanations of exit codes see <http://tldp.org/LDP/abs/html/exitcodes.html>
+ */
 class RunResult {
-  // See http://tldp.org/LDP/abs/html/exitcodes.html
-  // Accessed 2012-11-07
 
-  /// Ran successfully
+  /**
+   * Ran successfully
+   *
+   * Code 0
+   */
   static final RunResult SUCCESS = const RunResult._internal(0, 'SUCCESS');
 
-  /// C/C++ standard for bad usage. Hop was called incorrectly
+  /**
+   * Hop was called with bad arguments.
+   *
+   * Code 64
+   *
+   * See `EX_USAGE` in <http://www.retro11.de/ouxr/211bsd/usr/include/sysexits.h.html>
+   */
   static final RunResult BAD_USAGE = const RunResult._internal(64, 'BAD USAGE');
 
-  /// Task was was started and failed gracefully
+  /**
+   * Task was was started and failed gracefully
+   *
+   * Code 80
+   */
   static final RunResult FAIL = const RunResult._internal(80, 'FAIL');
 
-  /// Task was was started, but threw an exception
+  /**
+   * Task was was started, but threw an exception
+   *
+   * Code 81
+   */
   static final RunResult EXCEPTION = const RunResult._internal(81, 'EXCEPTION');
 
-  /// Task misbehaved. Returned null, used TaskContext incorrectly, etc.
+  /**
+   * Task misbehaved. Returned null, used [TaskContext] incorrectly, etc.
+   *
+   * Code 82
+   */
   static final RunResult ERROR = const RunResult._internal(82, 'ERROR');
 
   final int exitCode;
