dd30a0082b3214e7dad9d5c92442b7b9138d6250
Kevin Moore <kevin@thinkpixellab.com>
2013-03-03 18:56:13 -0500

    bot_io: AnsiColor learned BOLD, RESET

--- lib/src/bot_io/ansi_color.dart
+++ lib/src/bot_io/ansi_color.dart
@@ -4,7 +4,6 @@ part of bot_io;
  * [More details](https://en.wikipedia.org/wiki/ANSI_escape_code#Colors)
  */
 class AnsiColor {
-  const String _NO_COLOR = '\u001b[0m';
   static const AnsiColor BLACK = const AnsiColor._internal(0, 'black');
   static const AnsiColor RED = const AnsiColor._internal(1, 'red');
   static const AnsiColor GREEN = const AnsiColor._internal(2, 'green');
@@ -14,18 +13,56 @@ class AnsiColor {
   static const AnsiColor CYAN = const AnsiColor._internal(6, 'cyan');
   static const AnsiColor GRAY = const AnsiColor._internal(7, 'gray');
   static const AnsiColor LIGHT_RED = const AnsiColor._internal(91, 'light red');
+  static const AnsiColor BOLD = const AnsiColor._internal(null, 'bold', _srg: 1);
+  static const AnsiColor RESET = const AnsiColor._internal(null, 'reset');
 
-  final int id;
+  /**
+   * Use [foregroundId] instead.
+   */
+  @deprecated
+  int get id => foregroundId;
+
+  final int foregroundId;
   final String name;
+  final int _srg;
 
-  const AnsiColor._internal(this.id, this.name);
+  bool get isBold => _srg == 1;
 
-  String toString() => "$name ($id)";
+  const AnsiColor._internal(this.foregroundId, this.name, {this._srg: 0});
 
-  String wrap(String input) {
-    assert(input != null);
-    return "${shellValue}$input${_NO_COLOR}";
+  AnsiColor asBold() {
+    if(isBold) {
+      return this;
+    } else {
+      return new AnsiColor._internal(foregroundId, name, _srg: 1);
+    }
   }
 
-  String get shellValue => '\u001b[3${id}m';
+  String toString() {
+    final value = 'AnsiColor: $name';
+
+    if(isBold) {
+      return value.concat(' (BOLD)');
+    } else {
+      return value;
+    }
+  }
+
+  // TODO: maybe no-op here if 'this' is RESET?
+  String wrap(String input) {
+    assert(input != null);
+    return "${shellValue}$input${RESET.shellValue}";
+  }
+
+  String get shellValue {
+    final items = new List<String>();
+
+    items.add(_srg.toString());
+
+    if(foregroundId != null) {
+      items.add('3$foregroundId');
+    }
+
+    return '\u001b[${items.join(";")}m';
+  }
 }
