7da9849873c76993bba413b26b68d31b141e40db
Kevin Moore <kevin@thinkpixellab.com>
2013-03-19 16:32:50 -0400

    test: huge oops in test_dump_render_tree
    Must return the associated future so that the result is actually waited on...

--- test/test_dump_render_tree.dart
+++ test/test_dump_render_tree.dart
@@ -11,7 +11,7 @@ void main() {
   group('DumpRenderTree', () {
     browserTests.forEach((file) {
       test(file, () {
-        _runDrt(file);
+        return _runDrt(file);
       });
     });
   });
