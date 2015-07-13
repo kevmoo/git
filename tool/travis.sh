c11e07536fb0ae71fa2691380ac71d228b941370
Kevin Moore <kevmoo@google.com>
2015-07-13 15:57:07 -0700

    travis: only activate coveralls when coverage is run
    also run in debug mode

--- tool/travis.sh
+++ tool/travis.sh
@@ -7,7 +7,9 @@ pub run test
 
 # Install dart_coveralls; gather and send coverage data.
 if [ "$COVERALLS_TOKEN" ] && [ "$TRAVIS_DART_VERSION" = "stable" ]; then
+  pub global activate dart_coveralls
   pub global run dart_coveralls report \
     --exclude-test-files \
+    --debug \
     test/test_all.dart
 fi
