8df59353a7875151982416be554b2d2961b80130
Kevin Moore <kevmoo@google.com>
2015-07-13 15:51:54 -0700

    move to pkg/test, removed hop

--- test/test_all.dart
+++ test/test_all.dart
@@ -6,8 +6,6 @@ import 'parse_test.dart' as parse;
 import 'git_dir_test.dart' as git_dir;
 
 void main() {
-  groupSep = ' - ';
-
   group('GitDir', git_dir.main);
   group('parse', parse.main);
 }
--- tool/travis.sh
+++ tool/travis.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-# Fast fail the script on failures.   
+# Fast fail the script on failures.
 set -e
 
-dart --checked test/harness_console.dart
+pub run test
 
 # Install dart_coveralls; gather and send coverage data.
 if [ "$COVERALLS_TOKEN" ] && [ "$TRAVIS_DART_VERSION" = "stable" ]; then
   pub global run dart_coveralls report \
-    --token $COVERALLS_TOKEN \
-    --retry 2 \
     --exclude-test-files \
-    test/harness_console.dart
+    test/test_all.dart
 fi
