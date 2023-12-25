d44684fafd878da8e27c066cc05d3401dec22a0e
John McMahon <john.a.mcmahon30.ctr@mail.mil>
2023-12-25 17:29:40 -0500

    Add test for lightweight tags

---
+++ test/tag_test.dart
@@ -0,0 +1,25 @@
+
+import 'package:git/git.dart';
+import 'package:test/test.dart';
+
+import 'test_utils.dart';
+
+void main() {
+
+  test('Parse lightweight tag', () async {
+    final testDir = await createTempGitDir();
+
+    final contents = <String, String>{
+      'something': 'value'
+    };
+
+    await doDescriptorGitCommit(testDir, contents, 'Something');
+    final branchRef = await testDir.currentBranch();
+
+    await runGit(['tag', 'newTag', branchRef.sha],
+        processWorkingDir: testDir.path,);
+
+    final tagsFound = await testDir.tags().length;
+    expect(tagsFound, equals(1));
+  });
+}
