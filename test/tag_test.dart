9a451586ab29310641f8f2fdd1e04b052ec506a0
John McMahon <john.a.mcmahon30.ctr@mail.mil>
2023-12-25 21:49:44 -0500

    Add test for annotated tags

--- test/tag_test.dart
+++ test/tag_test.dart
@@ -22,4 +22,26 @@ void main() {
     final tagsFound = await testDir.tags().length;
     expect(tagsFound, equals(1));
   });
+
+  test('Parse annotated tag', () async {
+    final testDir = await createTempGitDir();
+
+    final contents = <String, String>{
+      'something': 'value'
+    };
+
+    await doDescriptorGitCommit(testDir, contents, 'Something');
+    final branchRef = await testDir.currentBranch();
+
+    await runGit(['tag', '--annotate', '--message', 'First tag', 'newTag',
+      branchRef.sha],
+        processWorkingDir: testDir.path,);
+
+    await runGit(['tag', '--annotate', '--message', 'First tag', 'anotherTag',
+      branchRef.sha],
+        processWorkingDir: testDir.path,);
+
+    final tagsFound = await testDir.tags().length;
+    expect(tagsFound, equals(2));
+  });
 }
