879421da4644be399a5d751baf14b325eec550bc
Stargator <7527155+Stargator@users.noreply.github.com>
2024-01-01 16:54:02 -0500

    Expand tag tests (#67)
    * Add check of tag content to lightweight tag test
    
    * Add check of tag content to annotated tag test
    
    * Swap out .first or .single
    
    Co-authored-by: Kevin Moore <kevmoo@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: John McMahon <john.a.mcmahon30.ctr@mail.mil>
    Co-authored-by: Kevin Moore <kevmoo@users.noreply.github.com>

--- test/tag_test.dart
+++ test/tag_test.dart
@@ -6,31 +6,39 @@ import 'test_utils.dart';
 void main() {
   test('Parse lightweight tag', () async {
     final testDir = await createTempGitDir();
-
     final contents = <String, String>{'something': 'value'};
+    const givenTagName = 'newTag';
 
     await doDescriptorGitCommit(testDir, contents, 'Something');
     final branchRef = await testDir.currentBranch();
 
     await runGit(
-      ['tag', 'newTag', branchRef.sha],
+      ['tag', givenTagName, branchRef.sha],
       processWorkingDir: testDir.path,
     );
 
-    final tagsFound = await testDir.tags().toList();
-    expect(tagsFound, hasLength(1));
+    final foundTag = (await testDir.tags().toList()).single;
+    expect(foundTag.tag, equals(givenTagName));
   });
 
   test('Parse annotated tag', () async {
     final testDir = await createTempGitDir();
-
     final contents = <String, String>{'something': 'value'};
+    const newTagName = 'newTag';
+    const anotherTagName = 'anotherTag';
 
     await doDescriptorGitCommit(testDir, contents, 'Something');
     final branchRef = await testDir.currentBranch();
 
     await runGit(
-      ['tag', '--annotate', '--message', 'First tag', 'newTag', branchRef.sha],
+      [
+        'tag',
+        '--annotate',
+        '--message',
+        'First tag',
+        newTagName,
+        branchRef.sha,
+      ],
       processWorkingDir: testDir.path,
     );
 
@@ -39,8 +47,8 @@ void main() {
         'tag',
         '--annotate',
         '--message',
-        'First tag',
-        'anotherTag',
+        'Second tag',
+        anotherTagName,
         branchRef.sha,
       ],
       processWorkingDir: testDir.path,
@@ -48,5 +56,10 @@ void main() {
 
     final tagsFound = await testDir.tags().toList();
     expect(tagsFound, hasLength(2));
+
+    final alphabeticallyFirstTag = tagsFound.first;
+    expect(alphabeticallyFirstTag.tag, equals(anotherTagName));
+    expect(alphabeticallyFirstTag.objectSha, equals(branchRef.sha));
+    expect(alphabeticallyFirstTag.type, equals('commit'));
   });
 }
